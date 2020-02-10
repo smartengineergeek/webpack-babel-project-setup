9c803b53ed23937a8e57d6d048799b70c379e3a0
Avinash Mahlawat <0004354@trulytravel.tech>
2020-02-10 16:37:08 +0000

    created minimal boilerplate

---
+++ Readme.md
@@ -0,0 +1,14 @@
+# Webpack Babel Project Setup
+
+In this project, we are setting up babel & Webpack manually without using a boilerplate.
+This  is based on tutorial from  https://www.robinwieruch.de/javascript-project-setup-tutorial
+
+It can be used as a front-end modern js boilerplate.
+
+## Installation
+
+* ``
+* cd webpack-babel-setup
+* npm install
+* npm start
+* visit `http://localhost:8080/`
---
+++ src/index.js
@@ -0,0 +1,18 @@
+import sum from './sum';
+
+console.log('Hello Webpack Project.');
+
+document.querySelector("#id1").innerHTML = `
+<center>
+    <h3>Webpack Babel Project Setup </h3>
+    <br/>
+    <p>
+        In this project, we are setting up babel & Webpack manually without using a boilerplate.
+    <br/>
+        This  is based on tutorial from  https://www.robinwieruch.de/javascript-project-setup-tutorial
+    <br/>
+        It can be used as a front-end modern js boilerplate.
+    </p>
+</center>
+`
+
